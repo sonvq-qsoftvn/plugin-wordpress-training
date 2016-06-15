27d688220bb5a7c3c51822e3f024bae14b578781
Steve <sonvq@qsoftvietnam.com>
2016-06-15 09:16:40 +0700

    commit custom-music-reviews

---
+++ wp-content/plugins/custom-music-reviews/custom-music-reviews.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Custom Music Reviews
+* Plugin URI: http://vuquangson.com/
+* Description: A custom music review plugin built for example
+* Version: 1.0
+* Author: Vu Quang Son
+* Author URI: http://vuquangson.com/
+**/
